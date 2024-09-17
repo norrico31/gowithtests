50407e5b90da906d1dee98962941a075bd5adb6f
norricogerald <norricobiason31@gmail.com>
2024-09-17 10:36:21 +0800

    refactor tests

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -24,15 +24,27 @@ func TestSearch(t *testing.T) {
 
 func TestAdd(t *testing.T) {
 	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
 		word := "test"
 		definition := "this is just a test"
-		dictionary := Dictionary{}
 
-		err := dictionary.Update(word, definition)
-
-		assertError(t, err, ErrWordDoesNotExist)
+		err := dictionary.Add(word, definition)
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
 	})
 
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+
+func TestUpdate(t *testing.T) {
 	t.Run("existing word", func(t *testing.T) {
 		word := "test"
 		definition := "this is just a test"
@@ -40,20 +52,18 @@ func TestAdd(t *testing.T) {
 		newDefinition := "new definition"
 
 		err := dictionary.Update(word, newDefinition)
-
 		assertError(t, err, nil)
 		assertDefinition(t, dictionary, word, newDefinition)
 	})
-}
 
-func TestUpdate(t *testing.T) {
-	word := "test"
-	definition := "this is just a test"
-	dictionary := Dictionary{word: definition}
-	newDefinition := "new definition"
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
 
-	dictionary.Update(word, newDefinition)
-	assertDefinition(t, dictionary, word, newDefinition)
+		err := dictionary.Update(word, definition)
+		assertError(t, err, ErrWordDoesNotExist)
+	})
 }
 
 func TestDelete(t *testing.T) {
